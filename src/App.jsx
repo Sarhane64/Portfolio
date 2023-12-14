0d3bee85033b823e1e8503c7453bb4cd202b3c07
jean519 <129189905+jean519@users.noreply.github.com>
2023-12-14 09:46:53 +0100

    initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {Outlet} from "react"
+
+function App() {
+  return (
+    
+  )
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
