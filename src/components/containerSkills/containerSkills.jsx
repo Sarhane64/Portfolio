88a234b1bcce60733b70a81d239b04f793f69a1f
jean519 <129189905+jean519@users.noreply.github.com>
2024-01-04 11:56:10 +0100

    commit

--- src/components/containerSkills/containerSkills.jsx
+++ src/components/containerSkills/containerSkills.jsx
@@ -8,7 +8,7 @@ function ContainerProjects() {
           link={"https://github.com/Sarhane64/quizV1"}
           icone={<img src="/github.svg" width={35} alt="github" />}
           optionlink={" https://funny-faloodeh-983ace.netlify.app/"}
-          optionIcon={<img src="/web.svg" width={35} alt="github" />}
+          optionIcon={<img src="/web.svg" width={35} alt="url" />}
           title={"Quiz"}
           text={`Réalisation d'un quiz, découverte
            de la gestion du DOM, le travail en équipe 
@@ -20,7 +20,7 @@ function ContainerProjects() {
         <div className="second-container-of-my-project">
           <Projet
             link={"https://github.com/Sarhane64/js-template-fullstack"}
-            icone={<img src="/github.svg" width={35} alt="javascript" />}
+            icone={<img src="/github.svg" width={35} alt="github" />}
             title={"Immobilier"}
             text={`Application sur le thème de la vente immobilière,
               le but est de mettre en place un CRUD et de la gestion d'une BDD .`}
@@ -34,11 +34,11 @@ function ContainerProjects() {
             link={
               "https://github.com/WildCodeSchool-2023-09/JS-RMT-React_If-Projet3-Wildy-Gamy"
             }
-            icone={<img src="/figma.svg" width={35} alt="github" />}
+            icone={<img src="/github.svg" width={35} alt="github" />}
             optionlink={
               "https://www.figma.com/file/vQMghf027CqugVVYcP43qH/Projet-wildy-gamy?type=design&node-id=0-1&mode=design&t=81VP2wGUHmFWZ0SK-0"
             }
-            optionIcon={<img src="/github.svg" width={35} alt="github" />}
+            optionIcon={<img src="/figma.svg" width={35} alt="figma" />}
             title={"Wildy-Gamy"}
             text={`Plateforme de jeu. Au départ, trois jeux classiques seront accessibles 
             (Memorize, Tic Tac ait et Word Typer). L'objectif est de mettre en place un 
